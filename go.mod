8f3245684b7a06f00d4d0156b5a6c394fa82c22c
Gustavo L de Mello (Guz) <contact@guz.one>
2025-01-29 11:24:10 -0300

    chore: initial commit, move from loreddev/x
    Moved the source code to a new repository outside from loreddev/x. To
    see the commit history before this commit, see
    https://forge.capytal.company/loreddev/x/src/commit/1f823aa0998d400c5ece22ca7b5af07965deeaac

---
+++ go.mod
@@ -0,0 +1,11 @@
+module forge.capytal.company/loreddev/blogo
+
+go 1.23.4
+
+require (
+	forge.capytal.company/loreddev/x v0.0.0-20250128201807-1f823aa0998d
+	github.com/yuin/goldmark v1.7.8
+	github.com/yuin/goldmark-meta v1.1.0
+)
+
+require gopkg.in/yaml.v2 v2.3.0 // indirect
---
+++ plugins/gitea/gitea_test.go
@@ -0,0 +1,32 @@
+package gitea_test
+
+import (
+	"io"
+	"testing"
+
+	"forge.capytal.company/loreddev/blogo/plugin"
+	"forge.capytal.company/loreddev/blogo/plugins/gitea"
+)
+
+func TestSource(t *testing.T) {
+	p := gitea.New("loreddev", "x", "https://forge.capytal.company")
+
+	s := p.(plugin.Sourcer)
+
+	fs, err := s.Source()
+	if err != nil {
+		t.Fatalf("Failed to source file system: %s %v", err.Error(), err)
+	}
+
+	file, err := fs.Open("blogo/LICENSE")
+	if err != nil {
+		t.Fatalf("Failed to open file: %s %v", err.Error(), err)
+	}
+
+	contents, err := io.ReadAll(file)
+	if err != nil {
+		t.Fatalf("Failed to read contents of file: %s %v", err.Error(), err)
+	}
+
+	t.Logf("Successfully read contents of file: %s", string(contents))
+}
